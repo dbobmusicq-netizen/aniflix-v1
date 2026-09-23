89adebab5d26ae3e11aa9f00fe175219aa2997b4
dbobmusicq-netizen <dbobmusicq@gmail.com>
2026-09-23 12:28:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * - Bi-directional Push Notification & Background Watch Sync Bridge.
  */
 
-const VERSION = '44.0.9';
+const VERSION = '45.0.9';
 const STATIC_CACHE = `animedrift-static-v${VERSION}`;
 const DYNAMIC_CACHE = `animedrift-dynamic-v${VERSION}`;
 const IMAGE_CACHE = `animedrift-images-v${VERSION}`;
